26d75ff8eb05f4324b82613be5cd168cdc75b064
Jinsu Jeong <104358180+JINSU-JEONG1@users.noreply.github.com>
2023-05-12 21:57:20 +0900

    upload file
    upload file

---
+++ src/main/java/com/gttn/jkGallery/JkGalleryApplication.java
@@ -0,0 +1,13 @@
+package com.gttn.jkGallery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JkGalleryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JkGalleryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gttn/jkGallery/JkGalleryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gttn.jkGallery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JkGalleryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
